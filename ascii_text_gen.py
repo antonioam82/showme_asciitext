096e40d62d1a0cbff61bfd6de777018ddc268035
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-10-12 16:16:53 +0200

    Update ascii_text_gen.py

--- ascii_text_gen.py
+++ ascii_text_gen.py
@@ -7,10 +7,16 @@ class app():
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("ASCII TEXT")
-        self.window.geometry("1178x718")
+        self.window.geometry("1180x718")
 
+        self.current_dir = tk.StringVar()
+        self.current_dir.set(os.getcwd())
+        
+        self.entryDir = tk.Entry(self.window,width=196,textvariable=self.current_dir)
+        self.entryDir.place(x=0,y=0)
 
         self.window.mainloop()
 
 if __name__=="__main__":
     app()
+
