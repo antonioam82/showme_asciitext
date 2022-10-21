d5054cf5e4165a26fb5d9770051af76d8da40f34
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-10-21 15:42:37 +0200

    Update ascii_text_gen.py

--- ascii_text_gen.py
+++ ascii_text_gen.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.scrolledtext as sct
 from tkinter import messagebox, filedialog
 import pyfiglet
 import os
@@ -14,9 +15,12 @@ class app():
         
         self.entryDir = tk.Entry(self.window,width=196,textvariable=self.current_dir)
         self.entryDir.place(x=0,y=0)
+        self.ascii_visor = sct.ScrolledText(self.window,width=110,height=30)
+        self.ascii_visor.place(x=10,y=30)
 
         self.window.mainloop()
 
 if __name__=="__main__":
     app()
+
 
